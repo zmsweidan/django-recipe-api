57e43fa1f221fa218dec1c706984ce4e81be2093
Zaid Sweidan <zmsweidan@gmail.com>
2020-10-04 18:27:27 -0400

    resolved unit test + lint issues

--- app/core/models.py
+++ app/core/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                        PermissionsMixin
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self._db) #for saving across on only 1 db#
+        user.save(using=self._db)  # for saving across on only 1 db#
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
@@ -22,6 +24,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
-
-

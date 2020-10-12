617a32afbb93383ae93e714585f8b6141dba7f62
Zaid Sweidan <zmsweidan@gmail.com>
2020-10-11 23:12:31 -0400

    modified build to include media directories

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework.routers import DefaultRouter
 
 from recipe import views
@@ -13,4 +15,4 @@ app_name = 'recipe'
 
 urlpatterns = [
     path('', include(router.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

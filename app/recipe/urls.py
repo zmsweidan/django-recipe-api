5ba21aa586a0a624a7fc18d72a33cedc51fb0380
Zaid Sweidan <zmsweidan@gmail.com>
2020-10-11 20:04:08 -0400

    added list recipe feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -3,10 +3,11 @@ from rest_framework.routers import DefaultRouter
 
 from recipe import views
 
-
+# Register view sets
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 
